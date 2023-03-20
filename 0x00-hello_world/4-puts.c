2ccb86d140d5f43cd9e605ff53f6202eb599dbde
MaxChush <september.max2909@gmail.com>
2023-03-20 03:07:05 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+  
 /**
- * main - this is the main function
+ * main - Entry point
  *
- *Return: always 0
+ * Description: prints "Programming is like building a multilingual puzzle"
  */
+
 int main(void)
 {
-puts('\"Programming is like building a multilingual puzzle):
-  return (0):
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
