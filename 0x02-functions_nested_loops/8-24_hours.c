f1cdc90322f3b104e438ba5851196feb2a8423b5
MaxChush <september.max2909@gmail.com>
2023-03-22 13:57:51 -0700

    8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,25 +1,24 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints 24 hour time
+ * jack_bauer - Prints 24 hours time
  *
  * return: always 0
  */
 
 void jack_bauer(void)
 {
-	int i;
-	int j;
+	int i, j;
 
-	for (i = 0 ; i < 24 ; i++)
+	for (i = 0; i <= 23; i++)
 	{
-		for (j = 0 ; j < 60 ; j++)
+		for (j = 0; j <= 59; j++)
 		{
-			_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
-			_putchar('i');
-			_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
 		}
 	}
